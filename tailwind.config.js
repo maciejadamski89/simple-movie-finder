5049ed755b74c2ca9c5386fc9a2e60ef5cfcab25
Maciej Adamski <maciej.adamski@ocean.ibm.com>
2023-02-20 14:07:06 +0100

    chore: add custom css for grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			gridTemplateColumns: {
+				fluid: "repeat(auto-fit, minmax(10rem, 1fr))",
+			},
+		},
 	},
 	plugins: [],
 };
